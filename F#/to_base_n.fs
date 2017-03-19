9f991abc6f28643dd71c22b9244e1e89a4233af2
CaoMengxi <3054428602@qq.com>
2017-03-19 22:55:06 +0800

    Add F#

---
+++ F#/to_base_n.fs
@@ -0,0 +1,20 @@
+﻿    printfn "Please enter base number:"
+    let mutable x = stdin.ReadLine()
+    printfn "Please enter system number:"
+    let mutable y = stdin.ReadLine()
+    printfn "The result is:"
+    let rec to_base_n bas n =
+        let mutable temp = 0
+        if n >= 36 then 
+            printfn "The system number is too large!!!"
+            ()
+        temp <- bas % n
+        if bas >= n then
+            let sab = bas / n
+            to_base_n sab n
+        if temp < 10 then
+            printf "%c" (char (48+temp))
+        else
+            printf "%c" (char (55+temp))
+    to_base_n (int x) (int y)
+    let o = stdin.ReadLine()
