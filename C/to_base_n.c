467b5bc7a94d9da32f2aa4084114f80ce15a2700
CaoMengxi <3054428602@qq.com>
2017-03-21 20:55:09 +0800

    Fix some bugs.

--- C/to_base_n.c
+++ C/to_base_n.c
@@ -4,22 +4,27 @@ int to_base_n(int base, int n);
 
 int main(void)
 {
-    int x, y;
+    int f=0;
+    double x,y;
     printf("Please enter base number and system number below (Use space to separate them):\n");
-    scanf("%d %d", &x, &y);
+    do
+    {
+        f=1;
+        scanf("\n%lf %lf", &x, &y);
+        if(x<0||y<=0||floor(x)!=x||floor(y)!=y||y>=36)
+            f=0;
+        if(f==0)
+            printf("You have input the wrong number, please try again:\n");
+    }
+    while(f==0);
     printf("The result is:\n");
-    to_base_n(x, y);
+    to_base_n((int)x, (int)y);
     while((getch()) == 'a');
 }
 
 int to_base_n(int base, int n)
 {
     int temp;
-    if(n >= 36)
-    {
-        printf("The system number is too large!!!");
-        return;
-    }
     temp = base % n;
     if(base >= n)
     {
